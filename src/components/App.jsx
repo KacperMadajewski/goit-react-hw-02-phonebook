a9a6aa0966b35f3567509361b6e5a0aee8546769
KacperMadajewski <kacpermadajewski@gmail.com>
2023-06-04 00:11:04 +0200

    Did something, don't know what

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,6 @@
 import React, { Component } from 'react';
 
 export class App extends Component {
-  state = {
-    contacts: [],
-    name: '',
-  };
-
-  handleNameChange = (evt) => {
-    this.setState({ name: evt.target.value });
-  }
-
-  handleSubmit = (evt) => {
-    evt.preventDefault();
-    this.setState({
-      contacts: [],
-      name: '',
-    });
-  }
-
   render() {
     return (
       <div
@@ -29,24 +12,7 @@ export class App extends Component {
           fontSize: 40,
           color: '#010101',
         }}
-      >
-        <form onSubmit={this.handleSubmit}>
-          <span>Name</span>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.handleNameChange}
-            value={this.state.name}
-          />
-          <button type='submit'>Add contact</button>
-        </form>
-        <ul>
-          <li>{this.state.name}</li>
-        </ul>
-      </div>
+      ></div>
     );
   }
 }
